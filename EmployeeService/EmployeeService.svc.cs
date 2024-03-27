c7efa2d7ce2ec85b36b8eb281b6729a4d7ac11d6
Dmitriy Alakoz <dalakoz@teamworkcommerce.com>
2024-03-28 00:21:16 +0200

    #comment rename Service1 -> EmployeeService #time 5m

--- EmployeeService/EmployeeService.svc.cs
+++ EmployeeService/EmployeeService.svc.cs
@@ -5,9 +5,7 @@ using EmployeeService.Models;
 
 namespace EmployeeService
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
-    public class Service1 : IEmployeeService
+    public class EmployeeService : IEmployeeService
     {
         private readonly IEmployeeStore _store = new EmployeeStore();
 
